eccb15b565bf0139afe14615f851b65a9dd94796
KrackenRoca <juanluisRoca360@gmail.com>
2019-09-11 11:19:27 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#mié sep 11 11:18:52 MDT 2019
+
+
+clients = 'name1,name2,'
+
+if __name__=='__main__':
+    clients += 'name3'
+    print(clients)
+
